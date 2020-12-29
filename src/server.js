2bc7b26db86bc94f839b7d8db9fe4877992f8962
César O. Araújo <cesar.araujo@mesainc.com.br>
2020-12-29 13:33:53 -0300

    chore: setting up mongoose, dotenv and creating database connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import routes from './routes';
+
+dotenv.config();
 
 const server = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect(
+  `mongodb+srv://ahoycap:${process.env.DB_PASSWORD}@cluster0.wimfi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, 
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
 
 server.use(express.json());
 server.use(routes);
